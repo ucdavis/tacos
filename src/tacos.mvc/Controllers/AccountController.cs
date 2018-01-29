b61bc09f1de27b2bf9aa0735c5655864c799c99a
Unknown <srkirkland@ucdavis.edu>
2018-01-29 14:21:18 -0800

    don't redirect to profile page

--- src/tacos.mvc/Controllers/AccountController.cs
+++ src/tacos.mvc/Controllers/AccountController.cs
@@ -352,9 +352,7 @@ namespace tacos.mvc.Controllers
                     {
                         await _signInManager.SignInAsync(user, isPersistent: false);
                         _logger.LogInformation("User created an account using {Name} provider.", info.LoginProvider);
-                        //return RedirectToLocal(returnUrl);
-                        TempData["Message"] = "Please update you profile before continuing.";
-                        return this.RedirectToAction("Edit", "Profile");
+                        return RedirectToLocal(returnUrl);
                     }
                 }
                 
