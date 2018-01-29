5a5868473b8990d1cfc36a28befbc5dbda6c0604
Unknown <srkirkland@ucdavis.edu>
2018-01-29 10:42:55 -0800

    db context using identity

--- src/tacos.mvc/Data/TacoDbContext.cs
+++ src/tacos.mvc/Data/TacoDbContext.cs
@@ -1,13 +1,16 @@
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 
 namespace tacos.data
 {
-    public class TacoDbContext : DbContext
+    public class TacoDbContext : IdentityDbContext<User>
     {
         public TacoDbContext(DbContextOptions<TacoDbContext> options)
             : base(options)
         { }
+
+        public new virtual DbSet<User> Users { get; set; }
         public virtual DbSet<Submission> Submissions { get; set; }
 
         public virtual DbSet<Request> Requests { get; set; }
