41c474839f4b7b639469f64c6145255285fe0c58
John Knoll <jpknoll@gmail.com>
2018-02-06 14:27:47 -0800

    users list

--- src/tacos.mvc/Controllers/SystemController.cs
+++ src/tacos.mvc/Controllers/SystemController.cs
@@ -1,12 +1,11 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using tacos.data;
+using tacos.mvc.Models.SystemViewModels;
 using tacos.mvc.Resources;
 
 namespace tacos.mvc.Controllers
@@ -14,17 +13,33 @@ namespace tacos.mvc.Controllers
     [Authorize(Roles = RoleCodes.Admin)]
     public class SystemController : ApplicationController
     {
+        private readonly TacoDbContext _dbContext;
         private readonly UserManager<User> _userManager;
 
-        public SystemController(UserManager<User> userManager)
+        public SystemController(TacoDbContext dbContext, UserManager<User> userManager)
         {
+            _dbContext = dbContext;
             _userManager = userManager;
         }
 
         [HttpGet]
-        public ActionResult Roles()
+        public async Task<ActionResult> Users()
         {
-            return View();
+            var users = _dbContext.Users.ToList();
+
+            var admins = (await _userManager.GetUsersInRoleAsync(RoleCodes.Admin));
+
+            var joined = from u in users
+                         join a in admins
+                         on u.Id equals a.Id into gj
+                         from match in gj.DefaultIfEmpty()
+                         select new UserRoleViewModel
+                         {
+                             User = u,
+                             IsAdmin = (match != null)
+                         };
+
+            return View(joined.ToList());
         }
 
         [HttpPost]
@@ -34,10 +49,7 @@ namespace tacos.mvc.Controllers
 
             await _userManager.AddToRoleAsync(user, role);
 
-            return new JsonResult(new
-            {
-                success = true
-            });
+            return RedirectToAction(nameof(Users));
         }
 
         [HttpPost]
@@ -47,10 +59,7 @@ namespace tacos.mvc.Controllers
 
             await _userManager.RemoveFromRoleAsync(user, role);
 
-            return new JsonResult(new
-            {
-                success = true
-            });
+            return RedirectToAction(nameof(Users));
         }
     }
 }
---
+++ src/tacos.mvc/Models/SystemViewModels/UserRoleViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using tacos.data;
+
+namespace tacos.mvc.Models.SystemViewModels
+{
+    public class UserRoleViewModel
+    {
+        public User User { get; set; }
+        public bool IsAdmin { get; set; }
+    }
+}
