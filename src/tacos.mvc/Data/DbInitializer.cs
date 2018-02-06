7e361744fe07f37525f56bd38f6c0dd3d9911760
John Knoll <jpknoll@gmail.com>
2018-02-06 13:30:07 -0800

    Setup Roles

--- src/tacos.mvc/Data/DbInitializer.cs
+++ src/tacos.mvc/Data/DbInitializer.cs
@@ -1,14 +1,51 @@
 using System.Security.Claims;
+using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Identity;
+using tacos.mvc.Resources;
 
 namespace tacos.data {
-    public static class DbInitializer {
-        public static async Task Initialize(TacoDbContext context, UserManager<User> userManager) {
+    public class DbInitializer
+    {
+        private readonly TacoDbContext _context;
+        private readonly UserManager<User> _userManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
 
-            context.Database.EnsureDeleted(); // TODO: remove after testing
-            context.Database.EnsureCreated();
+        public DbInitializer(TacoDbContext context, UserManager<User> userManager, RoleManager<IdentityRole> roleManager)
+        {
+            _context = context;
+            _userManager = userManager;
+            _roleManager = roleManager;
+        }
 
+        public async Task RecreateAndInitialize()
+        {
+            await _context.Database.EnsureDeletedAsync();
+
+            await Initialize();
+        }
+
+        public async Task Initialize() {
+
+            _context.Database.EnsureCreated();
+
+            await CreateRoles();
+
+            await CreateUsers();
+
+            CreateSubmissions();
+
+            _context.SaveChanges();
+        }
+
+        private async Task CreateRoles()
+        {
+            await _roleManager.CreateAsync(new IdentityRole(RoleCodes.Admin));
+            await _roleManager.CreateAsync(new IdentityRole(RoleCodes.User));
+        }
+
+        private async Task CreateUsers()
+        {
             var scottUser = new User
             {
                 Email = "srkirkland@ucdavis.edu",
@@ -17,24 +54,51 @@ namespace tacos.data {
                 Id = "postit"
             };
 
-            var userPrincipal = new ClaimsPrincipal();
-            userPrincipal.AddIdentity(new ClaimsIdentity(new[] {
+            var scottPrincipal = new ClaimsPrincipal();
+            scottPrincipal.AddIdentity(new ClaimsIdentity(new[]
+            {
                 new Claim(ClaimTypes.NameIdentifier, "postit"),
                 new Claim(ClaimTypes.Name, "Scott Kirkland")
             }));
 
-            var loginInfo = new ExternalLoginInfo(userPrincipal, AspNetCore.Security.CAS.CasDefaults.AuthenticationScheme, "postit", null);
+            var scottInfo = new ExternalLoginInfo(scottPrincipal, AspNetCore.Security.CAS.CasDefaults.AuthenticationScheme,
+                "postit", null);
 
-            await userManager.CreateAsync(scottUser);
-            await userManager.AddLoginAsync(scottUser, loginInfo);
- 
+            await _userManager.CreateAsync(scottUser);
+            await _userManager.AddLoginAsync(scottUser, scottInfo);
+            await _userManager.AddToRoleAsync(scottUser, RoleCodes.Admin);
+
+            var johnUser = new User()
+            {
+                Email = "jpknoll@ucdavis.edu",
+                UserName = "jpknoll",
+                Name = "John Knoll",
+                Id = "jpknoll"
+            };
+
+            var johnPrincipal = new ClaimsPrincipal();
+            johnPrincipal.AddIdentity(new ClaimsIdentity(new[]
+            {
+                new Claim(ClaimTypes.NameIdentifier, "jpknoll"),
+                new Claim(ClaimTypes.Name, "John Knoll"),
+            }));
+
+            var johnInfo = new ExternalLoginInfo(johnPrincipal, AspNetCore.Security.CAS.CasDefaults.AuthenticationScheme,
+                "jpknoll", null);
+
+            await _userManager.CreateAsync(johnUser);
+            await _userManager.AddLoginAsync(johnUser, johnInfo);
+            await _userManager.AddToRoleAsync(johnUser, RoleCodes.Admin);
+        }
+
+        private void CreateSubmissions()
+        {
             var submission = new Submission { Actor = "postit" };
-            var request = new Request { CourseNumber = "MAT 16", CalculatedTotal = 20.5  };
+            var request = new Request { CourseNumber = "MAT 16", CalculatedTotal = 20.5 };
 
             submission.Requests.Add(request);
 
-            context.Submissions.Add(submission);
-            context.SaveChanges();
+            _context.Submissions.Add(submission);
         }
     }
 }
---
+++ src/tacos.mvc/Resources/RoleCodes.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace tacos.mvc.Resources
+{
+    public static class RoleCodes
+    {
+        public const string Admin = "Admin";
+        public const string User = "User";
+    }
+}
