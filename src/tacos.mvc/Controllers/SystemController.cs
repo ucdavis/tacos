3fdc8c76340b2322b9cd07eda2e715f28b79fcae
John Knoll <jpknoll@gmail.com>
2018-02-06 13:30:18 -0800

    Roles Actions

---
+++ src/tacos.mvc/Controllers/SystemController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using tacos.data;
+using tacos.mvc.Resources;
+
+namespace tacos.mvc.Controllers
+{
+    [Authorize(Roles = RoleCodes.Admin)]
+    public class SystemController : ApplicationController
+    {
+        private readonly UserManager<User> _userManager;
+
+        public SystemController(UserManager<User> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        [HttpGet]
+        public ActionResult Roles()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> AddUserToRole(string userId, string role)
+        {
+            var user = await _userManager.FindByIdAsync(userId);
+
+            await _userManager.AddToRoleAsync(user, role);
+
+            return new JsonResult(new
+            {
+                success = true
+            });
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> RemoveUserFromRole(string userId, string role)
+        {
+            var user = await _userManager.FindByIdAsync(userId);
+
+            await _userManager.RemoveFromRoleAsync(user, role);
+
+            return new JsonResult(new
+            {
+                success = true
+            });
+        }
+    }
+}
