cea5d86b57ba51fe27aa3d2ab5a29f0e6660d508
Unknown <srkirkland@ucdavis.edu>
2018-02-07 15:42:40 -0800

    simple admin pages

---
+++ src/tacos.mvc/Controllers/AdminController.cs
@@ -0,0 +1,41 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using tacos.data;
+
+namespace tacos.mvc.Controllers
+{
+    [Authorize(Roles = "Admin")]
+    public class AdminController : ApplicationController
+    {
+        private readonly TacoDbContext dbContext;
+
+        public AdminController(TacoDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            // get most recent submissions
+            var submissions = await dbContext
+                .Submissions.OrderByDescending(s => s.Created).Take(100)
+                .AsNoTracking().ToArrayAsync();
+
+            return View(submissions);
+        }
+
+        public async Task<IActionResult> Edit(int id) {
+            // edit a specific submission
+            var submission = await dbContext.Submissions
+                .Include(s=>s.Requests).AsNoTracking().SingleAsync(x=>x.Id == id);
+
+            return View(submission);
+        }
+    }
+}
