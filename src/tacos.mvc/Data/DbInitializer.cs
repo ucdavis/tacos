bee3b63945ec9db112bcdbc66c7312d7d3556dbc
Unknown <srkirkland@ucdavis.edu>
2018-02-08 11:40:29 -0800

    show contest info on admin edit page

--- src/tacos.mvc/Data/DbInitializer.cs
+++ src/tacos.mvc/Data/DbInitializer.cs
@@ -95,7 +95,7 @@ namespace tacos.data {
         {
             var submission = new Submission { Actor = "postit" };
             var request = new Request { CourseNumber = "MAT16", CourseType = "STD", RequestType = "TA", CalculatedTotal = 2.5 };
-            var request2 = new Request { CourseNumber = "MAT17", CourseType = "LAB", RequestType = "TA", CalculatedTotal = 3.5 };
+            var request2 = new Request { CourseNumber = "MAT17", CourseType = "LAB", RequestType = "TA", CalculatedTotal = 3.5, Contested = true, ContestReason = "Because I am special", ContestTotal = 5.25 };
 
             submission.Requests.Add(request);
             submission.Requests.Add(request2);
