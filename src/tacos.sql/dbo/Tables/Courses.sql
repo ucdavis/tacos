d485d18968d7f56f975b9cd8b916878c7c3035e1
Scott Kirkland <srkirkland@gmail.com>
2018-03-05 16:55:40 -0800

    update sql project

---
+++ src/tacos.sql/dbo/Tables/Courses.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[Courses] (
+    [Number]                   NVARCHAR (450) NOT NULL,
+    [AverageEnrollment]        INT            NOT NULL,
+    [AverageSectionsPerCourse] FLOAT (53)     NOT NULL,
+    [Name]                     NVARCHAR (MAX) NULL,
+    [TimesOfferedPerYear]      FLOAT (53)     NOT NULL,
+    CONSTRAINT [PK_Courses] PRIMARY KEY CLUSTERED ([Number] ASC)
+);
+
--- src/tacos.sql/dbo/Tables/Requests.sql
+++ src/tacos.sql/dbo/Tables/Requests.sql
@@ -1,12 +1,19 @@
 ﻿CREATE TABLE [dbo].[Requests] (
     [Id]              INT            IDENTITY (1, 1) NOT NULL,
-    [Approved]        BIT            NOT NULL,
+    [Approved]        BIT            NULL,
     [CalculatedTotal] FLOAT (53)     NOT NULL,
+    [ContestReason]   NVARCHAR (MAX) NULL,
+    [ContestTotal]    FLOAT (53)     NOT NULL,
+    [Contested]       BIT            NOT NULL,
     [CourseNumber]    NVARCHAR (MAX) NULL,
+    [CourseType]      NVARCHAR (MAX) NULL,
+    [RequestType]     NVARCHAR (MAX) NULL,
     [SubmissionId]    INT            NOT NULL,
     CONSTRAINT [PK_Requests] PRIMARY KEY CLUSTERED ([Id] ASC),
     CONSTRAINT [FK_Requests_Submissions_SubmissionId] FOREIGN KEY ([SubmissionId]) REFERENCES [dbo].[Submissions] ([Id]) ON DELETE CASCADE
 );
+
+
 
 
 GO
