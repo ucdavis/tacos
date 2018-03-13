0d0c2547eb3b16d864dd64f935ddefa1f74a31a6
Scott Kirkland <srkirkland@gmail.com>
2018-03-13 10:30:05 -0700

    update submissions table

--- src/tacos.sql/dbo/Tables/Submissions.sql
+++ src/tacos.sql/dbo/Tables/Submissions.sql
@@ -1,7 +1,11 @@
 ﻿CREATE TABLE [dbo].[Submissions] (
-    [Id]      INT            IDENTITY (1, 1) NOT NULL,
-    [Actor]   NVARCHAR (MAX) NULL,
-    [Created] DATETIME2 (7)  NOT NULL,
+    [Id]         INT            IDENTITY (1, 1) NOT NULL,
+    [Actor]      NVARCHAR (MAX) NULL,
+    [ActorName]  NVARCHAR (MAX) NULL,
+    [Created]    DATETIME2 (7)  NOT NULL,
+    [Department] NVARCHAR (MAX) NULL,
     CONSTRAINT [PK_Submissions] PRIMARY KEY CLUSTERED ([Id] ASC)
 );
+
+
 
