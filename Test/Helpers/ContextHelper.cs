aabf9d5c9a8a92106f5e53e5b2bec0d7c06ba5df
Jason Sylvestre <jsylvestre@ucdavis.edu>
2018-04-03 09:06:44 -0700

    Test project
    Basic structure and a couple tests

---
+++ Test/Helpers/ContextHelper.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.Data.Sqlite;
+using Microsoft.EntityFrameworkCore;
+using tacos.data;
+
+namespace Test.Helpers
+{
+    public class ContextHelper : IDisposable
+    {
+        private SqliteConnection Connection { get; }
+        public TacoDbContext Context { get; }
+
+        public ContextHelper()
+        {
+            Connection = new SqliteConnection("DataSource=:memory:");
+            Connection.Open();
+
+            var options = new DbContextOptionsBuilder<TacoDbContext>()
+                .UseSqlite(Connection)
+                .Options;
+            Context = new TacoDbContext(options);
+            Context.Database.EnsureCreated();
+        }
+
+
+        public void Dispose()
+        {
+            Connection?.Close();
+        }
+    }
+}
