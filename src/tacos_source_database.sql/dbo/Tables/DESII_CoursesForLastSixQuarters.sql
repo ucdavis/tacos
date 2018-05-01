5ab73fee09592cf674727dfb11b4b2ec0b746221
Ken Taylor <kentaylor@ucdavis.edu>
2018-05-01 11:58:07 -0700

    Added Tacos source database project to Github project.
    Added Tacos source database project to Github project.

---
+++ src/tacos_source_database.sql/dbo/Tables/DESII_CoursesForLastSixQuarters.sql
@@ -0,0 +1,23 @@
+﻿CREATE TABLE [dbo].[DESII_CoursesForLastSixQuarters] (
+    [AcademicYear]         NVARCHAR (7)  NOT NULL,
+    [AcademicTermCode]     NVARCHAR (6)  NOT NULL,
+    [College]              NVARCHAR (40) NULL,
+    [DeptName]             NVARCHAR (40) NULL,
+    [SubjectCode]          NVARCHAR (4)  NOT NULL,
+    [CourseNumber]         NVARCHAR (7)  NOT NULL,
+    [CourseName]           VARCHAR (255) NULL,
+    [Enrollment]           INT           NULL,
+    [NumCreditSections]    INT           NULL,
+    [NumNonCreditSections] INT           NULL
+);
+
+
+GO
+CREATE CLUSTERED INDEX [DESII_CoursesForLastSixQuarters_SubjCodeCrseNumAcademicTermCode_CLSTRIDX]
+    ON [dbo].[DESII_CoursesForLastSixQuarters]([SubjectCode] ASC, [CourseNumber] ASC, [AcademicTermCode] DESC);
+
+
+GO
+CREATE NONCLUSTERED INDEX [DESII_CoursesForLastSixQuarters_AcademicTermCode_IDX]
+    ON [dbo].[DESII_CoursesForLastSixQuarters]([AcademicTermCode] DESC);
+
