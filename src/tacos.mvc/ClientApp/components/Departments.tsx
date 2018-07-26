b5c8205469fdfcff7a8f50460609b6a4e6b2b70b
Unknown <srkirkland@ucdavis.edu>
2018-07-26 10:41:03 -0700

    new departments

--- src/tacos.mvc/ClientApp/components/Departments.tsx
+++ src/tacos.mvc/ClientApp/components/Departments.tsx
@@ -18,9 +18,28 @@ export default class Departments extends React.PureComponent<IProps, {}> {
           value={this.props.department}
           onChange={e => this.props.onChange(e.target.value)}
         >
-          <option value="AANS">Animal Science</option>
-          <option value="APLS">Plant Sciences</option>
-          <option value="LAWR">Land, Air, Water</option>
+          <option value='AARE'>Agricultural & Resource Economics</option>
+          <option value='AANS'>Animal Science</option>
+          <option value='ABAE'>Biological & Agricultural Engineering</option>
+          <option value='ADNO'>CAES Deans Office</option>
+          <option value='AENM'>Entomology & Nematology</option>
+          <option value='ADES'>Environmental Science & Policy</option>
+          <option value='AETX'>Environmental Toxicology</option>
+          <option value='BEVE'>Evolution & Ecology</option>
+          <option value='AFST'>Food Science & Technology</option>
+          <option value='AHCE'>Human Ecology</option>
+          <option value='AIND'>Independent</option>
+          <option value='ALAW'>Land, Air & Water Resources</option>
+          <option value='BMIC'>Microbiology & Molecular Genetics</option>
+          <option value='BMCB'>Molecular & Cellular Biology</option>
+          <option value='BNPB'>Neurobiology, Physiology & Behavior</option>
+          <option value='ANUT'>Nutrition</option>
+          <option value='BPLB'>Plant Biology</option>
+          <option value='APPA'>Plant Pathology</option>
+          <option value='APLS'>Plant Sciences</option>
+          <option value='ATXC'>Textiles & Clothing</option>
+          <option value='AVIT'>Viticulture & Enology</option>
+          <option value='AWFC'>Wildlife, Fish & Conservation Biology</option>
         </select>
       </div>
     );
