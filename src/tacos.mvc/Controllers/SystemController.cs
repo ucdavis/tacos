abe357546c09d04344605160749b368c150ca04c
John Knoll <jpknoll@gmail.com>
2018-11-30 11:35:59 -0800

    manage department roles

--- src/tacos.mvc/Controllers/SystemController.cs
+++ src/tacos.mvc/Controllers/SystemController.cs
@@ -4,7 +4,9 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using tacos.data;
+using tacos.mvc.Data;
 using tacos.mvc.Models.SystemViewModels;
 using tacos.mvc.Resources;
 
@@ -23,32 +25,49 @@ namespace tacos.mvc.Controllers
         }
 
         [HttpGet]
-        public async Task<ActionResult> Users()
+        public async Task<IActionResult> Users()
         {
             var users = _dbContext.Users.ToList();
 
             var admins = (await _userManager.GetUsersInRoleAsync(RoleCodes.Admin));
             var reviewers = (await _userManager.GetUsersInRoleAsync(RoleCodes.Reviewer));
 
-            var joined = from u in users
-                         join a in admins
-                         on u.Id equals a.Id into gj
-                         join r in reviewers
-                         on u.Id equals r.Id into rj
-                         from match in gj.DefaultIfEmpty()
-                         from match2 in rj.DefaultIfEmpty()
-                         select new UserRoleViewModel
-                         {
-                             User = u,
-                             IsAdmin = (match != null),
-                             IsReviewer = (match2 != null),
-                         };
+            var systemRoles = from u in users
+                              join a in admins
+                              on u.Id equals a.Id into gj
+                              join r in reviewers
+                              on u.Id equals r.Id into rj
+                              from match in gj.DefaultIfEmpty()
+                              from match2 in rj.DefaultIfEmpty()
+                              select new SystemRoleViewModel()
+                              {
+                                  User = u,
+                                  IsAdmin = (match != null),
+                                  IsReviewer = (match2 != null),
+                              };
 
-            return View(joined.ToList());
+            var departmentRoles = await _dbContext.DepartmentRoles
+                .Include(r => r.Department)
+                .Include(r => r.User)
+                .AsNoTracking()
+                .ToListAsync();
+
+            var model = new UserRolesViewModel()
+            {
+                SystemRoles = systemRoles.ToList(),
+                DepartmentRoles = departmentRoles,
+            };
+
+            ViewBag.Departments = await _dbContext.Departments
+                .OrderBy(d => d.Name)
+                .AsNoTracking()
+                .ToListAsync();
+
+            return View(model);
         }
 
         [HttpPost]
-        public async Task<ActionResult> AddUserToRole(string userId, string role)
+        public async Task<IActionResult> AddUserToRole(string userId, string role)
         {
             var user = await _userManager.FindByIdAsync(userId);
 
@@ -58,11 +77,50 @@ namespace tacos.mvc.Controllers
         }
 
         [HttpPost]
-        public async Task<ActionResult> RemoveUserFromRole(string userId, string role)
+        public async Task<IActionResult> RemoveUserFromRole(string userId, string role)
         {
             var user = await _userManager.FindByIdAsync(userId);
 
             await _userManager.RemoveFromRoleAsync(user, role);
+
+            return RedirectToAction(nameof(Users));
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AddUserToDepartmentRole(string userId, int departmentId)
+        {
+            var user = await _userManager.FindByIdAsync(userId);
+
+            var department = await _dbContext.Departments
+                .Include(d => d.MemberRoles)
+                .FirstOrDefaultAsync(d => d.Id == departmentId);
+
+            _dbContext.DepartmentRoles.Add(new DepartmentRole()
+            {
+                User = user,
+                Department = department,
+                Role = "Member",
+            });
+
+            await _dbContext.SaveChangesAsync();
+
+            return RedirectToAction(nameof(Users));
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> RemoveUserToDepartmentRole(string userId, int departmentId)
+        {
+            var role = await _dbContext.DepartmentRoles
+                .FirstOrDefaultAsync(r => r.UserId == userId && r.DepartmentId == departmentId);
+
+            if (role == null)
+            {
+                return NotFound();
+            }
+
+            _dbContext.DepartmentRoles.Remove(role);
+
+            await _dbContext.SaveChangesAsync();
 
             return RedirectToAction(nameof(Users));
         }
---
+++ src/tacos.mvc/Models/SystemViewModels/UserRolesViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using tacos.data;
+using tacos.mvc.Data;
+
+namespace tacos.mvc.Models.SystemViewModels
+{
+    public class UserRolesViewModel
+    {
+        public IList<SystemRoleViewModel> SystemRoles { get; set; }
+
+        public IList<DepartmentRole> DepartmentRoles { get; set; }
+    }
+
+    public class SystemRoleViewModel
+    {
+        public User User { get; set; }
+        public bool IsAdmin { get; set; }
+        public bool IsReviewer { get; set; }
+    }
+}
