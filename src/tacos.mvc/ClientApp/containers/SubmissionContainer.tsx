9725844e8d7e0c99e0c363f0178a293acc28d124
John Knoll <jpknoll@gmail.com>
2018-12-03 13:54:59 -0800

    pass in department
    fix some styles

--- src/tacos.mvc/ClientApp/containers/SubmissionContainer.tsx
+++ src/tacos.mvc/ClientApp/containers/SubmissionContainer.tsx
@@ -6,259 +6,234 @@ import Departments from "../components/Departments";
 
 import { formulas } from "../util/formulas";
 
-import { IRequest } from '../models/IRequest';
+import * as LocalStorageService from "../services/LocalStorageService";
+
+import { IRequest } from "../models/IRequest";
 import { IDepartment } from "../models/IDepartment";
 import { ISubmission } from "ClientApp/models/ISubmission";
 
 interface IProps {
-  departments: IDepartment[];
-  requests?: IRequest[];
+    department: IDepartment;
+    requests?: IRequest[];
 }
 
 interface IState {
-  selectedDepartmentId: number;
-  requests: IRequest[];
+    requests: IRequest[];
 }
 
 export default class SubmissionContainer extends React.Component<IProps, IState> {
-  constructor(props: IProps) {
-    super(props);
+    constructor(props: IProps) {
+        super(props);
 
-    const selectedDepartmentId = props.departments[0].id;
+        let requests = LocalStorageService.getRequests(props.department);
+        if (!requests.length) {
+            requests = props.requests || [];
+        }
 
-    this.state = {
-      selectedDepartmentId,
-      requests: props.requests || [],
+        this.state = {
+            requests: requests
+        };
+    }
+
+    public render() {
+        return (
+            <div>
+                {this.renderRequests()}
+                <Summary
+                    canSubmit={this.isValidSubmission()}
+                    total={this.submissionTotal()}
+                    onSubmit={this.submit}
+                    onReset={this.onReset}
+                />
+            </div>
+        );
+    }
+
+    private submissionTotal = () => {
+        const { requests } = this.state;
+
+        // go add up everything they have requested
+        const total = requests.reduce((acc, req) => {
+            // add in exception total if exception, otherwise the annualized total
+            const value = req.exception ? req.exceptionAnnualizedTotal : req.annualizedTotal;
+            return acc + value;
+        }, 0);
+
+        return total;
     };
-  }
 
-  componentDidMount() {
-    // reload state from local storage
-    const existingRequestString = localStorage.getItem("requests");
+    private onReset = () => {
+        const { department } = this.props;
 
-    if (existingRequestString) {
-      this.setState({ requests: JSON.parse(existingRequestString) });
-    } else {
-      this.onAddRequest(); // add a starter one
-    }
+        // reset the form, clear storage
+        if (confirm("Are you sure you want to clear this form and start over?")) {
+            LocalStorageService.clearRequests(department);
+            this.setState({ requests: [] });
+        }
+    };
 
-    const selectedDepartmentId = localStorage.getItem("selectedDepartmentId");
+    private isValidSubmission = (): boolean => {
+        // make sure we have at least one request
+        if (this.state.requests.length === 0) {
+            return false;
+        }
 
-    if (selectedDepartmentId) {
-      this.setState({ selectedDepartmentId: parseInt(selectedDepartmentId, 10) });
-    }
-  }
+        // submission is valid if every course is valid and every exception has a valid exceptionTotal
+        return this.state.requests.every(
+            r =>
+                !!r.course &&
+                !!r.course.number &&
+                (!r.exception || (r.exception && r.exceptionTotal >= 0))
+        );
+    };
 
-  public render() {
-    const { departments } = this.props;
-    const { selectedDepartmentId } = this.state;
+    private submit = async () => {
+        try {
+            const { department } = this.props;
+            const { requests } = this.state;
 
-    return (
-      <div>
-        <Departments
-          departmentId={selectedDepartmentId}
-          departments={departments}
-          onChange={this.onDepartmentChange}
-        />
-        {this.renderRequests()}
-        <Summary
-          canSubmit={this.isValidSubmission()}
-          total={this.submissionTotal()}
-          onSubmit={this.submit}
-          onReset={this.onReset}
-        />
-      </div>
-    );
-  }
+            // filter just dirty submittions
+            const dirtyRequests = requests.filter(r => r.isDirty);
 
-  private onDepartmentChange = (value: number) => {
-    this.setState({ selectedDepartmentId: value });
+            // create the submission
+            const submission: ISubmission = {
+                departmentId: department.id,
+                requests: dirtyRequests
+            };
 
-    localStorage.setItem("selectedDepartmentId", `${value}`);
-  };
+            const response = await fetch("/requests/submit", {
+                body: JSON.stringify(submission),
+                headers: [["Accept", "application/json"], ["Content-Type", "application/json"]],
+                method: "POST",
+                credentials: "include"
+            });
 
-  private submissionTotal = () => {
-    const { requests } = this.state;
+            if (!response.ok) {
+                throw new Error(response.statusText);
+            }
 
-    // go add up everything they have requested
-    const total = requests.reduce((acc, req) => {
-      // add in exception total if exception, otherwise the annualized total
-      const value = (req.exception ? req.exceptionAnnualizedTotal : req.annualizedTotal);
-      return acc + value;
-    }, 0);
+            const result = await response.json();
 
-    return total;
-  };
+            // TODO: make sure we have success
+            LocalStorageService.clearRequests(department);
+            window.location.replace("/requests");
+        } catch (err) {
+            console.error(err);
+        }
+    };
 
-  private onReset = () => {
-    // reset the form, clear storage
-    if (confirm("Are you sure you want to clear this form and start over?")) {
-      localStorage.removeItem("requests");
-      localStorage.removeItem("selectedDepartmentId");
-      this.setState({ requests: [] });
-    }
-  };
+    private requestUpdated = (i: number, request: IRequest) => {
+        const { department } = this.props;
 
-  private isValidSubmission = (): boolean => {
-    // make sure we have at least one request
-    if (this.state.requests.length === 0) {
-      return false;
-    }
+        const annualizationRatio = 4.0 / 12.0;
+        // if the course info looks good, calculate totals
+        request.calculatedTotal = formulas[request.courseType].calculate(request.course);
 
-    // submission is valid if every course is valid and every exception has a valid exceptionTotal
-    return this.state.requests.every(
-      r =>
-        (!!r.course && !!r.course.number) &&
-        (!r.exception || (r.exception && r.exceptionTotal >= 0))
-    );
-  };
+        request.exceptionAnnualizedTotal =
+            request.exceptionTotal * annualizationRatio * request.course.timesOfferedPerYear;
 
-  private submit = async () => {
+        request.annualizedTotal =
+            request.calculatedTotal * annualizationRatio * request.course.timesOfferedPerYear;
 
-    try {
-      const { selectedDepartmentId, requests } = this.state;
-  
-      // create the submission
-      const submission: ISubmission = {
-        departmentId: selectedDepartmentId,
-        requests: requests
-      };
-  
-      const response = await fetch("/requests/submit", {
-        body: JSON.stringify(submission),
-        headers: [
-          ["Accept", "application/json"],
-          ["Content-Type", "application/json"]
-        ],
-        method: "POST",
-        credentials: "include"
-      });
+        request.isDirty = true;
 
-      if (!response.ok) {
-        throw new Error(response.statusText);
-      }
+        const requests = this.state.requests;
+        requests[i] = request;
 
-      const result = await response.json();
+        LocalStorageService.saveRequests(department, requests);
+        this.setState({ requests });
+    };
 
-      // TODO: make sure we have success
-      localStorage.removeItem("requests");
-      window.location.replace("/requests");
-    }
-    catch (err) {
-      console.error(err);
-    }
-  };
+    private removeRequest = (i: number) => {
+        const { department } = this.props;
+        console.log("removing request");
 
-  private requestUpdated = (i: number, request: IRequest) => {
-    const annualizationRatio = 4.0 / 12.0;
-    // if the course info looks good, calculate totals
-    request.calculatedTotal = formulas[request.courseType].calculate(
-      request.course
-    );
+        const requests = [...this.state.requests];
+        requests.splice(i, 1);
 
-    request.exceptionAnnualizedTotal = request.exceptionTotal * annualizationRatio * request.course.timesOfferedPerYear;
+        LocalStorageService.saveRequests(department, requests);
+        this.setState({ requests });
+    };
 
-    request.annualizedTotal =
-      request.calculatedTotal * annualizationRatio * request.course.timesOfferedPerYear;
+    private renderRequests = () => {
+        const requestList = this.state.requests.map((req, i) => (
+            <Request
+                key={`course-${i}`}
+                request={req}
+                index={i}
+                onEdit={this.requestUpdated}
+                onRemove={this.removeRequest}
+            />
+        ));
 
-    const requests = this.state.requests;
-    requests[i] = request;
+        return (
+            <table className="table">
+                <thead>
+                    <tr>
+                        <th>Course Number</th>
+                        <th>
+                            Course Type &nbsp;&nbsp;
+                            <a target="_blank" href="/CAES-TA-Guidelines 2018-21.pdf">
+                                Criteria Info <i className="fas fa-external-link-alt" />
+                            </a>
+                        </th>
+                        <th>
+                            <span
+                                data-toggle="tooltip"
+                                data-placement="top"
+                                title="For courses that require both TAs and Readers, select the majority position type."
+                            >
+                                Request Type <i className="fas fa-question-circle" />
+                            </span>
+                        </th>
+                        <th>TAs per course</th>
+                        <th>Annual TA FTE</th>
+                        <th>Exception?</th>
+                        <th>Remove</th>
+                    </tr>
+                </thead>
+                {requestList}
+                <tfoot>
+                    <tr>
+                        <td colSpan={5}>
+                            <button
+                                className="btn btn-primary"
+                                id="add-new"
+                                onClick={this.onAddRequest}
+                            >
+                                Add New
+                            </button>
+                        </td>
+                    </tr>
+                </tfoot>
+            </table>
+        );
+    };
 
-    localStorage.setItem("requests", JSON.stringify(requests));
-
-    this.setState({ requests });
-  };
-
-  private removeRequest = (i: number) => {
-    console.log("remove request");
-
-    const requests = [...this.state.requests];
-    requests.splice(i, 1);
-
-    localStorage.setItem("requests", JSON.stringify(requests));
-
-    this.setState({
-      requests
-    });
-  };
-
-  private renderRequests = () => {
-    const requestList = this.state.requests.map((req, i) => (
-      <Request
-        key={`course-${i}`}
-        request={req}
-        index={i}
-        onEdit={this.requestUpdated}
-        onRemove={this.removeRequest}
-      />
-    ));
-
-    return (
-      <table className="table">
-        <thead>
-          <tr>
-            <th>Course Number</th>
-            <th>
-              Course Type
-              &nbsp;&nbsp;
-                <a target="_blank" href="/CAES-TA-Guidelines 2018-21.pdf">
-                  Criteria Info  <i className="fas fa-external-link-alt"></i>
-                </a>
-                
-            </th>
-            <th>
-              <span data-toggle="tooltip" data-placement="top" title="For courses that require both TAs and Readers, select the majority position type.">
-                Request Type <i className="fas fa-question-circle"></i>
-              </span>
-            </th>
-            <th>TAs per course</th>
-            <th>Annual TA FTE</th>
-            <th>Exception?</th>
-            <th>Remove</th>
-          </tr>
-        </thead>
-        {requestList}
-        <tfoot>
-          <tr>
-            <td colSpan={5}>
-              <button
-                className="btn btn-primary"
-                id="add-new"
-                onClick={this.onAddRequest}
-              >
-                Add New
-              </button>
-            </td>
-          </tr>
-        </tfoot>
-      </table>
-    );
-  };
-
-  private onAddRequest = () => {
-    const requests = [
-      ...this.state.requests,
-      {
-        course: {
-          name: "",
-          number: "",
-          timesOfferedPerYear: 0,
-          averageEnrollment: 0,
-          averageSectionsPerCourse: 0,
-          valid: false
-        },
-        courseType: "STD",
-        requestType: "TA",
-        calculatedTotal: 0,
-        annualizedTotal: 0,
-        exception: false,
-        exceptionReason: "",
-        exceptionTotal: 0.00,
-        exceptionAnnualizedTotal: 0,
+    private onAddRequest = () => {
+        const requests: IRequest[] = [
+            ...this.state.requests,
+            {
+                course: {
+                    name: "",
+                    number: "",
+                    timesOfferedPerYear: 0,
+                    averageEnrollment: 0,
+                    averageSectionsPerCourse: 0
+                },
+                courseNumber: "",
+                courseType: "STD",
+                requestType: "TA",
+                calculatedTotal: 0,
+                annualizedTotal: 0,
+                exception: false,
+                exceptionReason: "",
+                exceptionTotal: 0.0,
+                exceptionAnnualizedTotal: 0,
                 isDirty: true
-      }
-    ];
+            }
+        ];
 
-    this.setState({ requests });
-  };
+        this.setState({ requests });
+    };
 }
