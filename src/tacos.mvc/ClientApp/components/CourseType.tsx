cdf8e2f6aaf1facc57bceeb85e103c04d8fe3fc7
John Knoll <jpknoll@gmail.com>
2018-12-20 09:51:16 -0800

    code style

--- src/tacos.mvc/ClientApp/components/CourseType.tsx
+++ src/tacos.mvc/ClientApp/components/CourseType.tsx
@@ -1,15 +1,15 @@
 import * as React from "react";
 
 interface IProps {
-  courseType: string;
-  onChange: (courseType: string) => void;
+    courseType: string;
+    onChange: (courseType: string) => void;
 }
 
 // render a textbox for inputing course number, or show course info if already selected
 export default class CourseType extends React.PureComponent<IProps, {}> {
-  public render() {
-    return (
-      <div className="input-group">
+    public render() {
+        return (
+            <div className="input-group">
                 <select
                     className="custom-select"
                     value={this.props.courseType}
@@ -17,16 +17,18 @@ export default class CourseType extends React.PureComponent<IProps, {}> {
                 >
                     <option value="STD">Standard lecture</option>
                     <option value="WRT">Writing intensive lecture</option>
-          <option value="LAB">Lab or Studio classes</option>
-          <option value="FLD">Field classes</option>
-          <option value="AUTO">Lecture only, automated grading</option>
-          <option value="MAN">Lecture only, manual grading</option>
-          <option value="MODW">Lecture only, moderate writing</option>
-          <option value="INT">Lecture only, writing intensive or substantial project</option>
-        </select>
-      </div>
-    );
-  }
+                    <option value="LAB">Lab or Studio classes</option>
+                    <option value="FLD">Field classes</option>
+                    <option value="AUTO">Lecture only, automated grading</option>
+                    <option value="MAN">Lecture only, manual grading</option>
+                    <option value="MODW">Lecture only, moderate writing</option>
+                    <option value="INT">
+                        Lecture only, writing intensive or substantial project
+                    </option>
+                </select>
+            </div>
+        );
+    }
 
     private onChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
         this.props.onChange(e.target.value);
