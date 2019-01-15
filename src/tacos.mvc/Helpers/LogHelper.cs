dfc37636c79e49e55bb50cda5c6055ff0460fcfd
John Knoll <jpknoll@gmail.com>
2019-01-15 10:23:12 -0800

    add logging

---
+++ src/tacos.mvc/Helpers/LogHelper.cs
@@ -0,0 +1,69 @@
+using System;
+using System.Diagnostics;
+using Microsoft.Extensions.Configuration;
+using Serilog;
+using Serilog.Exceptions;
+using StackifyLib;
+
+namespace tacos.mvc.Helpers
+{
+    public static class LogHelper
+    {
+        private static bool _loggingSetup;
+
+        private static IConfigurationRoot _configuration;
+
+        /// <summary>
+        /// Configure Application Logging
+        /// </summary>
+        public static void Setup(IConfigurationRoot configuration)
+        {
+            if (_loggingSetup) return; //only setup logging once
+
+            // save configuration for later calls
+            _configuration = configuration ?? throw new ArgumentNullException(nameof(configuration));
+
+            // create global logger with standard configuration
+            Log.Logger = GetConfiguration().CreateLogger();
+
+            AppDomain.CurrentDomain.UnhandledException += (sender, e) => Log.Fatal(e.ExceptionObject as Exception, e.ExceptionObject.ToString());
+
+            AppDomain.CurrentDomain.ProcessExit += (sender, e) => Log.CloseAndFlush();
+
+#if DEBUG
+            Serilog.Debugging.SelfLog.Enable(msg => Debug.WriteLine(msg));
+#endif
+
+            _loggingSetup = true;
+        }
+        /// <summary>
+        /// Get a logger configuration that logs to stackify
+        /// </summary>
+        /// <returns></returns>
+        public static LoggerConfiguration GetConfiguration()
+        {
+            if (_configuration == null) throw new InvalidOperationException("Call Setup() before requesting a Logger Configuration"); ;
+
+            // standard logger
+            var logConfig = new LoggerConfiguration()
+                .Enrich.FromLogContext()
+                .Enrich.WithExceptionDetails();
+
+            // various sinks
+            logConfig = logConfig
+                .WriteToStackifyCustom();
+
+            return logConfig;
+        }
+
+        private static LoggerConfiguration WriteToStackifyCustom(this LoggerConfiguration logConfig)
+        {
+            if (!_loggingSetup)
+            {
+                _configuration.ConfigureStackifyLogging();
+            }
+
+            return logConfig.WriteTo.Stackify();
+        }
+    }
+}
