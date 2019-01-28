6e047bfcd07f28afc3f846fe03c047290cbe3234
John Knoll <jpknoll@gmail.com>
2019-01-28 10:18:47 -0800

    webpack 4

--- src/tacos.mvc/webpack.config.js
+++ src/tacos.mvc/webpack.config.js
@@ -1,82 +1,124 @@
 const path = require("path");
 const webpack = require("webpack");
-const ExtractTextPlugin = require("extract-text-webpack-plugin");
-const UglifyJsPlugin = require("uglifyjs-webpack-plugin");
-const CheckerPlugin = require("awesome-typescript-loader").CheckerPlugin;
-const bundleOutputDir = "./wwwroot/dist";
+
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CheckerPlugin = require('awesome-typescript-loader').CheckerPlugin;
+const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin");
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+
+const bundleOutputDir = './wwwroot/dist';
 
 module.exports = env => {
-  const isDevBuild = !(env && env.prod);
-  return [
-    {
-      stats: { modules: false },
-      entry: {
-        app: "./ClientApp/app.tsx",
-        edit: "./ClientApp/pages/EditSubmission.tsx",
-        vendor: [
-          "event-source-polyfill",
-          "isomorphic-fetch",
-          "react",
-          "react-dom"
-        ]
-      },
-      resolve: { extensions: [".js", ".jsx", ".ts", ".tsx"] },
-      output: {
-        path: path.join(__dirname, bundleOutputDir),
-        filename: "[name].js",
-        publicPath: "/"
-      },
-      module: {
-        rules: [
-          {
-            test: /\.tsx?$/,
-            include: /ClientApp/,
-            use: "awesome-typescript-loader?silent=true"
-          },
-          {
-            test: /\.css$/,
-            use: isDevBuild
-              ? ["style-loader", "css-loader"]
-              : ExtractTextPlugin.extract({ use: "css-loader?minimize" })
-          },
-          {
-            test: /\.scss$/,
-            use: isDevBuild
-              ? ["style-loader", "css-loader", "sass-loader"]
-              : ExtractTextPlugin.extract({
-                  use: ["css-loader", "sass-loader"]
-                })
-          },
-          { test: /\.(png|jpg|jpeg|gif|svg|woff)$/, use: "url-loader?limit=25000" }
-        ]
-      },
-      plugins: [
-        new CheckerPlugin(),
-        new webpack.DefinePlugin({
-          "process.env.NODE_ENV": isDevBuild ? '"development"' : '"production"'
-        }),
-        new webpack.optimize.CommonsChunkPlugin({
-          name: "vendor",
-          minChunks: Infinity
-        })
-      ].concat(
-        isDevBuild
-          ? [
-              // Plugins that apply in development builds only
-              new webpack.SourceMapDevToolPlugin({
-                filename: "[file].map", // Remove this line if you prefer inline source maps
-                moduleFilenameTemplate: path.relative(
-                  bundleOutputDir,
-                  "[resourcePath]"
-                ) // Point sourcemap entries to the original file locations on disk
-              })
+    const isDevBuild = !(env && env.prod);
+    const isAnalyze = (env && env.analyze);
+
+    return [{
+        stats: {
+            modules: false
+        },
+        entry: {
+            app: "./ClientApp/app.tsx",
+            edit: "./ClientApp/pages/EditSubmission.tsx",
+        },
+        resolve: {
+            extensions: [".js", ".jsx", ".ts", ".tsx"]
+        },
+        output: {
+            path: path.join(__dirname, bundleOutputDir),
+            filename: "[name].js",
+            publicPath: "/"
+        },
+        mode: isDevBuild ? 'development' : 'production',
+        devtool: isDevBuild ? 'eval-source-map': 'source-map',
+        module: {
+            rules: [{
+                    test: /\.tsx?$/,
+                    include: /ClientApp/,
+                    use: "awesome-typescript-loader?silent=true"
+                },
+                {
+                    test: /\.css$/,
+                    use: [
+                        !isDevBuild ?
+                        MiniCssExtractPlugin.loader : {
+                            loader: 'style-loader',
+                            options: {
+                                sourceMap: true,
+                            },
+                        },
+                        {
+                            loader: 'css-loader',
+                            options: {
+                                sourceMap: true,
+                            },
+                        },
+                    ]
+                },
+                {
+                    test: /\.scss$/,
+                    use: [
+                        !isDevBuild ?
+                        MiniCssExtractPlugin.loader : {
+                            loader: 'style-loader',
+                            options: {
+                                sourceMap: true,
+                            },
+                        },
+                        {
+                            loader: 'css-loader',
+                            options: {
+                                sourceMap: true,
+                            },
+                        },
+                        {
+                            loader: 'sass-loader',
+                            options: {
+                                sourceMap: true,
+                            },
+                        },
+                    ]
+                },
+                {
+                    test: /\.(png|jpg|jpeg|gif|svg|woff)$/,
+                    use: "url-loader?limit=25000"
+                }
             ]
-          : [
-              // Plugins that apply in production builds only
-              new UglifyJsPlugin(),
-              new ExtractTextPlugin("site.css")
-            ]
-      )
-    }
-  ];
+        },
+        optimization: {
+            minimizer: isDevBuild ? [] : [
+                new UglifyJsPlugin({
+                    cache: true,
+                    parallel: true,
+                    sourceMap: true,
+                }),
+                new OptimizeCSSAssetsPlugin({}),
+            ],
+            splitChunks: {
+                chunks: 'all',
+                cacheGroups: {
+                    default: false,
+                    vendor: {
+                        name: 'vendor',
+                        test: /[\\/]node_modules[\\/]/,
+                        priority: -10
+                    },
+                }
+            }
+        },
+        plugins: [
+            new CheckerPlugin(),
+            ...isDevBuild ? [
+                // Plugins that apply in development builds only
+            ] : [
+                // Plugins that apply in production builds only
+                new MiniCssExtractPlugin({
+                    filename: 'site.min.css',
+                }),
+            ],
+            // Webpack Bundle Analyzer
+            // https://github.com/th0r/webpack-bundle-analyzer
+            ...isAnalyze ? [new BundleAnalyzerPlugin()] : [],
+        ],
+    }];
 };
