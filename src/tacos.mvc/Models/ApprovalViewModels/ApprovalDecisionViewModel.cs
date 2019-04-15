866d510fa783cdba1fbecb230a00bc94162568b4
John Knoll <jpknoll@gmail.com>
2019-04-15 16:54:22 -0700

    add dropdown for exception ux

--- src/tacos.mvc/Models/ApprovalViewModels/ApprovalDecisionViewModel.cs
+++ src/tacos.mvc/Models/ApprovalViewModels/ApprovalDecisionViewModel.cs
@@ -1,7 +1,4 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace tacos.mvc.Models.ApprovalViewModels
 {
@@ -10,5 +7,7 @@ namespace tacos.mvc.Models.ApprovalViewModels
         public bool Approved { get; set; }
 
         public string Comment { get; set; }
+
+        public string CommentOther { get; set; }
     }
 }
