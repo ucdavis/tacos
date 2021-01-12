75e896aa98f1b4fb43036dbecf2b70b1466e6e2b
Scott Kirkland <srkirkland@gmail.com>
2021-01-12 11:10:27 -0800

    setup defaults

--- src/tacos.mvc/Program.cs
+++ src/tacos.mvc/Program.cs
@@ -37,6 +37,8 @@ namespace tacos.mvc
             var loggingSection = configuration.GetSection("Stackify");
                 
             Log.Logger = new LoggerConfiguration()
+                .MinimumLevel.Override("Microsoft", LogEventLevel.Information)
+                // .MinimumLevel.Override("Microsoft.EntityFrameworkCore", LogEventLevel.Warning) // uncomment this to hide EF core general info logs
                 .MinimumLevel.Override("Microsoft.AspNetCore", LogEventLevel.Warning)
                 .Enrich.FromLogContext()
                 .Enrich.WithExceptionDetails()
