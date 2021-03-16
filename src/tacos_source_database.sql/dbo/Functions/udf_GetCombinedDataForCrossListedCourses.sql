b3ab153135fb137dcc6a98acde2c91d28cbf9ee8
Ken Taylor <kentaylor@ucdavis.edu>
2021-03-15 17:09:42 -0700

    Inverted course taught in most recent year bit, plus added missing sprocs, etc.

---
+++ src/tacos_source_database.sql/dbo/Functions/udf_GetCombinedDataForCrossListedCourses.sql
@@ -0,0 +1,147 @@
+﻿-- =============================================
+-- Author:		Ken Taylor
+-- Create date: October 17, 2018
+-- Description:	Combine the enrollment and sections data
+--	for all the cross-listed courses identified in the
+--  course catalog for the past two academic years.
+--
+-- Prerequsites: [dbo].[DESII_CoursesForLastSixQuarters] table must have been loaded.
+--	[dbo].[CourseDescription] table must have been loaded.
+--
+-- Usage:
+/*
+	USE [Tacos]
+	GO
+
+	DELETE FROM Tacos.dbo.LoadingCourses WHERE IsCrossListed = 1
+	INSERT INTO Tacos.dbo.LoadingCourses (SubjectCode, CourseNumber, DeptName, Number, Name, AverageEnrollment, AverageSectionsPerCourse, TimesOfferedPerYear, IsCrossListed)
+	SELECT * FROM [dbo].[udf_GetCombinedDataForCrossListedCourses]()
+
+	GO
+*/
+-- Modifications:
+-- =============================================
+CREATE FUNCTION udf_GetCombinedDataForCrossListedCourses 
+()
+RETURNS @CrossListedCourseTotals TABLE (
+	[SubjectCode] [nvarchar](4) NOT NULL,
+	[CourseNumber] [nvarchar](7) NOT NULL,
+	[DeptName] [nvarchar](50) NULL,
+	[Number] [nvarchar](15) NOT NULL,
+	[Name] [nvarchar](255) NULL,
+	[AverageEnrollment] [float] NULL,
+	[AverageSectionsPerCourse] [float] NULL,
+	[TimesOfferedPerYear] [float] NULL,
+	[IsCrossListed] [bit] NULL
+)
+AS
+BEGIN
+    -- We use this value so we don't get course info for a course
+	-- that was offered in a future term, meaning a term code that
+	-- occured after the two full academic years in question.
+	DECLARE @MaxTermCode int = (SELECT MAX(TermCode) FROM  
+	[dbo].[udf_GetTermCodeTableForPriorTwoAcademicYears](NULL))
+
+	DECLARE myCursor CURSOR FOR
+	SELECT 
+		 t3.AcademicYear
+		,t3.AcademicTermCode
+		,t1.[Course]
+		,t1.[SubjectCode]
+		,t1.[CourseNumber]
+		,t1.Title
+		,u.CrossListing
+		,Enrollment
+		,NumCreditSections
+		,NumNonCreditSections
+  FROM [Tacos].[dbo].[CourseDescription] t1
+  CROSS APPLY [dbo].[udf_ParseOutCrossListedCourses] (
+	t1.[Course], t1.CrossListing
+	) u
+  INNER  JOIN [dbo].[DESII_CoursesForLastSixQuarters] t3 ON t1.SubjectCode = t3.SubjectCode
+	AND (t1.CourseNumber = t3.CourseNumber OR t1.Course = u.CrossListing) 
+  WHERE t1.[CrossListing]  IS NOT NULL AND Status = 'Active'
+  ORDER BY 1, 3
+
+  DECLARE @CommonRows TABLE ( 
+	AcademicYear varchar(10), 
+	AcademicTermCode varchar(10), 
+	Course varchar(10), 
+    SubjectCode varchar(3), 
+	CourseNumber varchar(5), 
+	Title varchar(MAX), 
+	CrossListing varchar(10), 
+    Enrollment int, 
+	NumCreditSections int, 
+	NumNonCreditSections int
+  )
+
+  DECLARE @AcademicYear varchar(10), @AcademicTermCode varchar(10), 
+	  @Course varchar(10), @SubjectCode varchar(3), @CourseNumber varchar(5),
+	  @Title varchar(MAX), @CrossListing varchar(10), @Enrollment int, 
+	  @NumCreditSections int, @NumNonCreditSections int
+
+  OPEN myCursor
+  FETCH NEXT FROM myCursor INTO @AcademicYear, @AcademicTermCode, @Course, 
+	@SubjectCode, @CourseNumber, @Title, @CrossListing, @Enrollment, 
+    @NumCreditSections , @NumNonCreditSections 
+
+  WHILE @@FETCH_STATUS <> -1
+  BEGIN
+	DECLARE @RowsPresent int
+    SELECT @RowsPresent = (
+		SELECT COUNT(*)  
+		FROM @CommonRows t1
+		WHERE t1.AcademicYear = @AcademicYear AND 
+			t1.AcademicTermCode = @AcademicTermCode AND
+			t1.Course = @Course AND 
+			t1.Enrollment = @Enrollment AND 
+			t1.NumCreditSections = @NumCreditSections AND
+			t1.NumNonCreditSections = @NumNonCreditSections
+		)
+
+	-- Insert the primary row for each academic year:
+	IF @RowsPresent = 0
+		INSERT INTO @CommonRows (AcademicYear, AcademicTermCode, Course, SubjectCode, CourseNumber,
+			CrossListing, Enrollment, NumCreditSections, NumNonCreditSections)
+		VALUES (@AcademicYear, @AcademicTermCode, @Course, @SubjectCode, @CourseNumber, --@Title, -- We can't use this title,
+	    -- because some of the titles for the cross-listed courses are slightly different due to punctuation and misspelt words.
+			@Course, @Enrollment, @NumCreditSections , @NumNonCreditSections)
+
+		--Every entry will refer to a cross-listed course, so add it to the table:
+	INSERT INTO @CommonRows (AcademicYear, AcademicTermCode, Course, SubjectCode, CourseNumber, --Title, -- We can't use this title,
+	-- because some of the titles for the cross-listed courses are slightly different due to punctuation and misspelt words.
+		CrossListing, Enrollment, NumCreditSections, NumNonCreditSections)
+	VALUES (@AcademicYear, @AcademicTermCode, @Course, @SubjectCode, @CourseNumber, --@Title, 
+		@CrossListing, @Enrollment, @NumCreditSections , @NumNonCreditSections)
+
+	FETCH NEXT FROM myCursor INTO @AcademicYear, @AcademicTermCode, @Course , @SubjectCode, @CourseNumber, @Title, 
+		@CrossListing, @Enrollment, @NumCreditSections, @NumNonCreditSections 
+  END
+  CLOSE myCursor
+  DEALLOCATE myCursor
+
+  -- Calculate the averages, set the titles and return the list of combined cross-listed courses:
+  INSERT INTO @CrossListedCourseTotals
+  SELECT 
+	LEFT(t1.CrossListing,4) SubjectCode, 
+	SUBSTRING(t1.CrossListing, 5,10) CourseNumber, 
+	NULL AS DeptName,
+	REPLACE(t1.CrossListing, ' ', '') AS [Number], 
+	t3.Title AS [Name], 
+	CONVERT(float, SUM(t1.Enrollment)) / CONVERT(float, SUM(t1.NumCreditSections)) AS AvgEnrollment,
+	CONVERT(float, SUM(t1.NumNonCreditSections)) / CONVERT(float, SUM(t1.NumCreditSections)) AS AvgSectionsPerCourse,
+	CONVERT(float, SUM(t1.NumCreditSections)) / 2 AS TimesOfferedPerYear,
+	1 AS IsCrosslisted
+  FROM @CommonRows t1
+  INNER JOIN [dbo].[DESII_CoursesForLastSixQuarters] t2 ON 
+	t1.CrossListing = t2.SubjectCode + ' ' + t2.CourseNumber -- This is necessary so we don't get an 
+	--	entry for a cross-listing that was not offered in the past 6 quarters.
+  LEFT OUTER JOIN CourseDescription t3 ON t1.CrossListing = t3.Course -- We add the title back to the record in this step.
+  GROUP BY t1.CrossListing, t3.Title
+  HAVING (SUM(t1.NumCreditSections) > 0)
+  ORDER BY 1
+
+  RETURN 
+
+END
---
+++ src/tacos_source_database.sql/dbo/Stored Procedures/usp_UpdateDataForCrossListedCourses.sql
@@ -0,0 +1,182 @@
+﻿-- =============================================
+-- Author:		Ken Taylor
+-- Create date: February 3, 2021
+-- Description:	Update the data for the cross-listed courses.
+-- Prerequsites: The Courses table must have already been loaded with
+--	the base course data.
+-- Notes:
+/*
+Logic for populating the data for cross listed courses:
+   Note that the courses table must have already been loaded, as we
+   use the AverageEnrollment present in that table.
+
+	Input:
+	1. A list of all of the crosslisted courses, and their average enrollment from the courses table.
+	2. A list of the crosslisted courses and their crosslistings.
+
+	Output:
+	1. Crosslisted courses with their sum of the Average Enrollment for the course itself and 
+	each of its crosslistings.  This sum will be the new Average Enrollment for the crosslisted
+	course and its corresponding crosslistings.
+*/
+-- Usage:
+
+/*
+	USE [Tacos]
+	GO
+
+	EXEC usp_UpdateDataForCrossListedCourses
+
+*/
+-- Modifications:
+--
+
+-- =============================================
+CREATE PROCEDURE usp_UpdateDataForCrossListedCourses 
+	-- Add the parameters for the stored procedure here
+AS
+BEGIN
+	-- SET NOCOUNT ON added to prevent extra result sets from
+	-- interfering with SELECT statements.
+	SET NOCOUNT ON;
+
+    DECLARE @CommonRows TABLE (
+	Course varchar(10),
+	AverageEnrollment float,
+	CombinedAverageEnrollment float,
+	CrossListing varchar(10),
+	CrossListingsString varchar(50),
+	IsCrossListed bit,
+	IsBaseCourse bit -- This allows us to easily determine the base row.
+  )
+
+	-- We're using the course description table for this as it
+	--	will contain all the active courses, regardless of their
+	--	enrollment values.
+	-- We will not worry about whether or not a course was taught within
+	-- the past 2 years at this time, because one of it's other sectons might have 
+	-- been, and we'll figure that out using the courses table.
+
+	-- @CommonRows contains each course (n times, one entry for each cross listing), 
+	-- and the cross-listed course's individual cross listing(s), 
+	-- plus the original non-parsed cross listing field from the CourseDescription table.
+	-- Note that this does not add a row for the base course as a cross-listed entry, so we'll
+	-- need to account for this later.
+
+	-- Add record for each of the cross-listed courses:
+	  INSERT INTO @CommonRows (Course, CrossListing, CrossListingsString, 
+	  IsCrossListed, IsBaseCourse)
+	  SELECT REPLACE(t1.Course, ' ', '') Course, 
+		REPLACE(t2.CrossListing, ' ', ''), t1.CrossListing, 
+		1 AS IsCrossListed,
+		0 AS IsBaseCourse
+	  FROM CourseDescription t1
+	  CROSS APPLY  [dbo].[udf_ParseOutCrossListedCourses] (
+		t1.[Course], t1.CrossListing
+		) t2
+	  WHERE t1.[CrossListing]  IS NOT NULL AND 
+		Status = 'Active' --AND t1.Course = 'EBS 148'
+	  ORDER BY t1.Course
+
+	  -- Add a record for each of the base courses:
+	  INSERT INTO @CommonRows (
+			Course, 
+			CrossListing,
+			CrossListingsString,
+			IsCrossListed,
+			IsBaseCourse
+		)
+		SELECT DISTINCT Course, Course AS CrossListing, CrossListingsString, 
+		1 AS IsCrossListed, 1 AS IsBaseCourse
+		FROM @CommonRows t1
+		ORDER BY Course, CrossListing, CrossListingsString
+
+	 -- Set the AverageEnrollment for the base courses:
+	  UPDATE @CommonRows
+	  SET AverageEnrollment = t2.NonCrossListedAverageEnrollment
+	  FROM @CommonRows t1
+	  INNER JOIN Courses t2 ON t1.Course = t2.Number
+
+	  -- This logic sets the average enrollment for the courses' cross-listed 
+	  -- segments, which are currently set to the base course' AverageEnrollment:  
+	  -- (See previous example.)
+
+	UPDATE @CommonRows
+	SET AverageEnrollment = t2.AverageEnrollment
+	FROM @CommonRows t1
+	INNER JOIN (
+		SELECT t2.Course, t2.AverageEnrollment, t1.CrossListing
+		FROM @CommonRows t1
+		INNER JOIN @CommonRows t2 ON 
+			t1.CrossListing = t2.Course
+	) t2 ON t1.CrossListing = t2.Course
+
+	-- Now we perform the actual update of the combined average enrollment:
+	UPDATE @CommonRows
+	SET CombinedAverageEnrollment = CombinedEnrollment
+	FROM @CommonRows t1
+	INNER JOIN (
+		SELECT Course, SUM(AverageEnrollment) CombinedEnrollment
+		FROM @CommonRows
+		GROUP BY Course
+	) t2 ON t1.Course = t2.Course
+
+ ---- Check to make sure things happened as we had intended:
+ -- SELECT Course, AverageEnrollment AS NonCrossListedAverageEnrollment,
+	--CombinedAverageEnrollment AS AverageEnrollment,
+	--CrossListingsString, IsCrossListed
+ -- FROM @CommonRows
+ -- WHERE IsBaseCourse = 1
+ -- ORDER BY Course
+
+  -- Second to last is to update the actual Courses table with all of the cross-listed course data:
+	UPDATE Courses
+	SET NonCrossListedAverageEnrollment = t2.NonCrossListedAverageEnrollment,
+		AverageEnrollment = t2.CombinedAverageEnrollment,
+		IsCrossListed = t2.IsCrossListed,
+		CrossListingsString = t2.CrossListingsString
+	FROM Courses t1
+	INNER JOIN (
+		SELECT Course, t1.AverageEnrollment AS NonCrossListedAverageEnrollment, 
+			t1.IsCrossListed, t1.CrossListingsString,t1.CombinedAverageEnrollment 
+		FROM @CommonRows t1
+		INNER JOIN Courses t2 ON t1.Course = t2.Number
+		WHERE IsBaseCourse = 1  
+	) t2 ON t1.Number = t2.Course
+
+	-- Last is to take care of some final housekeeping:
+
+	-- Since we've identified all of the cross listed courses that are active, but
+	-- not offered within the past 2 years, and other active, but not offered within
+	-- the past 2 years courses must not be cross listed courses.
+	-- Therefore, we can update any remaing null IsCrossListed fields with 0,
+	-- as these courses can assumed not to be cross listed.
+	--
+	UPDATE Courses
+	SET IsCrossListed = 0
+	WHERE IsCrossListed IS NULL
+
+	-- Populate the department name as this may have yet to be done:
+	-- (I think there was/is an issue with updating the department name 
+	-- using data present in the registration data as the department name can
+	-- (and does) differ between one year and the next, and this caused duplicate
+	-- entries.
+	-- Making the update here using the CourseDescription table eliminates that 
+	-- possibility, as the CourseDescription table only contains a single entry
+	-- for each active course, and hence, only a single Department.)
+
+	UPDATE Courses
+	SET DeptName = t2.Department
+	FROM Courses t1
+	INNER JOIN ( 
+		SELECT t2.*, t1.* 
+		FROM Courses t1
+		LEFT OUTER JOIN (
+			SELECT DISTINCT Course, Department
+			FROM CourseDescription
+			WHERE Status = 'Active'
+		) t2 ON t1.Number = REPLACE(Course, ' ' ,'')
+	) t2 ON t1.Number = t2.Number
+	WHERE t1.DeptName IS NULL AND t2.Department IS NOT NULL
+
+END
